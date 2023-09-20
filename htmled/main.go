5f0539d8cdf8a17d55cfc58eaf108c38f6044f1c
mdalp <marius@alpmann.de>
2023-09-20 18:58:57 +0200

    Change htmled syntax message to common syntax formatting style

--- htmled/main.go
+++ htmled/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		panic("Illegal number of arguments. Syntax: htmled [html file] [query selector]")
+		panic("Illegal number of arguments. Syntax: htmled html-file query-selector")
 	}
 
 	filepath := os.Args[1]
