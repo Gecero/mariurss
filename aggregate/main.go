b431a122368300f3f43c3b77386ec5e5a0e79c16
mdalp <marius@alpmann.de>
2023-10-04 23:02:49 +0200

    Restructure aggregate to parse in parralel

--- aggregate/main.go
+++ aggregate/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/mmcdole/gofeed"
 )
-
-func readLines() ([]string, error) {
-	stdin, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return nil, err
-	}
-	lines := []string{}
-	for _, line := range strings.Split(string(stdin), "\n") {
-		if len(line) != 0 {
-			lines = append(lines, line)
-		}
-	}
-	return lines, nil
-}
 
 func crampString(str string, bounds int) string {
 	indicator := "..."
@@ -40,48 +27,67 @@ type News struct {
 	feed *gofeed.Feed
 }
 
-func main() {
-	feeds := []*gofeed.Feed{}
-	files, err := readLines()
+func parseNewsFileAsync(path string, news chan News, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	file, err := os.Open(path)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, "Error opening file:", err)
 	}
-	// Read all files
-	for _, filepath := range files {
-		file, err := os.Open(filepath)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Can't open file:", err)
-			continue
-		}
-		parser := gofeed.NewParser()
-		feed, err := parser.Parse(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Could not parse feed in ", filepath, ":", err)
-			continue
-		} else {
-			feeds = append(feeds, feed)
-		}
+	defer file.Close()
+
+	parser := gofeed.NewParser()
+	feed, err := parser.Parse(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error parsing file:", err)
 	}
 
-	// Create list containting all feed items
+	for _, item := range feed.Items {
+		news <- News{item, feed}
+	}
+}
+
+func collectNewsItems(newsDB *[]News, newsChan chan News) {
+	for {
+		*newsDB = append(*newsDB, <-newsChan)
+	}
+}
+
+func readAndParse() []News {
 	news := []News{}
-	for _, feed := range feeds {
-		for _, itm := range feed.Items {
-			news = append(news, News{itm, feed})
-		}
+	newsChan := make(chan News)
+	var wg sync.WaitGroup
+	// This'll read from 'newsChan' to 'news' while avoiding race condition
+	go collectNewsItems(&news, newsChan)
+
+	// Read line-by-line, parse each file async
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		wg.Add(1)
+		go parseNewsFileAsync(scanner.Text(), newsChan, &wg)
 	}
 
-	// Sort by date
-	sort.Slice(news, func(i, j int) bool {
-		timeI := news[i].item.PublishedParsed
-		timeJ := news[j].item.PublishedParsed
+	wg.Wait()
+	return news
+}
+
+func sortNews(news *[]News) {
+	// Sort by date, newest at the top
+	sort.Slice(*news, func(i, j int) bool {
+		timeI := (*news)[i].item.PublishedParsed
+		timeJ := (*news)[j].item.PublishedParsed
 		return timeI.Compare(*timeJ) == 1
 	})
+}
 
+func writeTable(news *[]News) {
 	// Write HTML code to stdout
 	// Table columns: Title (Link) and Date/Feed, Description
-	feedHTML := "<table><tr><th>News</th><th>Description</th></tr>\n"
-	for _, new := range news {
+	// Important: First line is table header, following lines are one
+	//            news entry each, last line is table closing tag
+	fmt.Println("<table><tr><th>News</th><th>Description</th></tr>")
+	for _, new := range *news {
+		var line string
 		reader := strings.NewReader(new.item.Content)
 		contentNode, err := goquery.NewDocumentFromReader(reader)
 		var contentText string
@@ -94,21 +100,26 @@ func main() {
 			contentText = crampString(contentText, 350)
 		}
 
-		feedHTML += "<tr>"
+		line += "<tr>"
 
-		feedHTML += "<td class='mariurss-content-main'>"
-		feedHTML += "<a class='mariurss-content-title' href='" + new.item.Link + "'>"
-		feedHTML += new.item.Title + "</a><br>"
-		feedHTML += "<span class='mariurss-content-date'>(" + new.item.PublishedParsed.Format(time.RFC1123) + ")</span>"
-		feedHTML += "<br><a class='mariurss-content-feed' href='" + new.feed.Link + "'>"
-		feedHTML += new.feed.Title + "</a></td>"
+		line += "<td class='mariurss-content-main'>"
+		line += "<a class='mariurss-content-title' href='" + new.item.Link + "'>"
+		line += new.item.Title + "</a><br>"
+		line += "<span class='mariurss-content-date'>(" + new.item.PublishedParsed.Format(time.RFC1123) + ")</span>"
+		line += "<br><a class='mariurss-content-feed' href='" + new.feed.Link + "'>"
+		line += new.feed.Title + "</a></td>"
 
-		feedHTML += "<td class='mariurss-content-description'><p>" + contentText + "</p></td>"
+		line += "<td class='mariurss-content-description'><p>" + contentText + "</p></td>"
 
-		feedHTML += "</tr>\n"
+		line += "</tr>"
+		fmt.Println(line)
 	}
-	feedHTML += "</table>"
 
-	fmt.Println(feedHTML)
+	fmt.Println("</table>")
+}
 
+func main() {
+	news := readAndParse()
+	sortNews(&news)
+	writeTable(&news)
 }
